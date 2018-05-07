dc723c9fe296b3520222c6da1c75adacc12f1703
newuser <newuser@mail.com>
2018-05-07 23:29:00 +0200

    feat (showtime) was on voc and add dialog feat (workflow) project detail

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# pages
+
+
 doc
 ftp://ftp.traduc.org/pub/lgazette/html/2004/101/lg101-P.html
 https://linux.die.net/man/1/dialog
@@ -31,6 +34,43 @@ project page
     txt: run terraform/ansible to provision it and see it live         
     
     
-ansible page
+showtime page
     txt: let's go crazy and deploy it on Amazon ! 
     input: amazon access key and access token for terraform
+    
+    
+# workflows  
+
+* select some technologies to see related project
+* select project to display details
+* showtime: WAE on VOC
+* showtime: VOC on Amazon   
+
+## info only
+
+## show time VOC on Amazon
+* using terraform, provision on Amazon an EC2 and the correct access group
+   * create terraform config 
+* using Ansible, provision VOC (with one runner)
+   * get runner token from user/password
+* seed a test project (like a simple web server with a nice html) to deploy on local mode
+   * see what's done for WAE
+
+## show time WAE on VOC
+
+| VOC is already fully setup with DNS records (needed for the mail)
+
+* ~~create normal gitlab user~~
+* ~~create empty project~~
+* push repo with
+  * simple-stack-instance on remote repo mode
+  * .gitlab -ci.yml
+* explain what does it do with WAE 
+* explain how to use WAE  
+
+
+
+
+
+
+
--- data/techno-project.sh
+++ data/techno-project.sh
@@ -3,24 +3,57 @@
 # define data
 declare -A techno_projects
 declare -A desc_projects
+declare -A detail_projects
 
 techno_projects[voc]="docker gitlab swarm node"
 desc_projects[voc]="VOC: automatic build and deployment for Swarm based on Gitlab CI"
+detail_projects[voc]="
+https://github.com/remipichon/voc
+Voc is Docker close friend, it can build and deploy on Docker Swarm
+
+Voc is so easy to enable. If you already have a Gitlab 8 and a Gitlab Runner defined, you just need to create a new runner using the Voc Runner NodeJs App available on Docker Hub and voilà ! If you don't have Gitlab already, which is bad, you can easily have it with Voc all configured via the given Ansible playbook that does the heavy lifting.
+
+Voc is stunning by its simplicity. With a few configuration in json format you can build and push images and deploy from docker compose file. Voc is awesome, yes, but it's partly because it uses another wonderful tool; Gitlab with CI enable. Each time a config or any Docker related files is committed, the Voc Runner App is triggered to apply just what it needs, smartly detecting with files are related to which Voc instance. So smart, it also monitor the context or your configured images via Dockerfile and can build and deploy your master branch each time a merge request is accepted.
+Voc should start making the coffee, let's create a feature request...
+
+But first, would you like to see it live? Better, to see it being provisioned on Amazon Web Service via Terraform then Ansible? Hit Showtime !"
 
 techno_projects[manifmaker]="node front"
 desc_projects[manifmaker]="ManifMaker: stuff to assign"
+detail_projects[manifmaker]="
+"
 
 techno_projects[cloudbase]="docker swarm groovy"
 desc_projects[cloudbase]="CloudbaseV2: custom orchestration tool for Swarm"
+detail_projects[cloudbase]="
+"
 
 techno_projects[wae]="front elastic spring"
 desc_projects[wae]="WhatStat: web app to display statistics for Whatsapp chats"
+detail_projects[wae]="
+"
 
 techno_projects[app]="android"
 desc_projects[app]="24Heure Android: application to provide live information to festival attendees"
+detail_projects[app]="
+"
 
 techno_projects[toast]="android python"
 desc_projects[toast]="ToastShooter: my first software, an Android replica of an iOs exclusive game"
+detail_projects[toast]="
+I didn't know Git at the time. No visioning, it makes it even more impressive.
+A funny catchy looking smartphone game which consist of shooting toasts jumping out of toasters.
+
+This project have a nice story. Have a tea with some biscuits and appreciate.
+It's my first project ever, my discovery of the magnifique world of computing. I used to play a game on iOs, ToastShooter, for hours. I was scoring around 2000 then 4000, sometimes 600 but rarely. It was a challenge de beat my record. One day, my thumbs slipped on my screen until I reached 1600, which I couldn't ever beat. It was done, I lost the envy to play that game with one impossible challenge. I also lost my iPhone and the game doesn't exist on Android.
+
+Then started the new challenge; play it once more, on Android.
+I used a Python library that could export to apk for Android; PyGame. I developed it, my specs being my memories of the game I knew by heart. I implemented all the rules, the timings, the points systems and even shamely took the graphics from Google. It was running at 240 fps on my patato laptop, 30 fps on my Android, joy ! It was really joy but it didn't play it, instead I conceived some new type of game. My favorite was the one where you are a splashed egg and you have to jump from toast to toast using the accelerometer to lead your jump. Careful to the burnt toasts !
+
+I still find it impressive, since I moved to the web and then to Android in Java which was so much move convenient but no game anymore.
+
+I can try to offer it to you, via a Docker image and XTerm forwarding, hope it works.
+"
 
 # Define the dialog exit status codes
 : ${DIALOG_OK=0}
@@ -55,18 +88,11 @@ function technoView(){
     exec 3>&-
 }
 
-
 function projectDetailView(){
     exec 3>&1
     result=$(dialog --clear --title "More about ${selected_project}" \
      --yes-label "Showtime !" --no-label "Go back to project list"  --yesno \
-     "Github or TOP_SECRET
-        one sentence summary
-        description:
-            what ?
-            user ?
-            how ?
-            with who ?" 30 100 2>&1 1>&3)
+     "${detail_projects["${selected_project}"]}" 30 100 2>&1 1>&3)
 
     return_value=$?
     exec 3>&-
@@ -201,7 +227,10 @@ projectDetailController(){
 }
 
 
-technoController
+#technoController
+
+selected_project=toast
+projectDetailController
 
 
 
