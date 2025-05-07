5d15d5b93cdb54eafe63f2b3059723d992d3a2c8
Remi Pichon <remi.pichon@ekwateur.fr>
2025-05-07 20:01:46 +0200

    kja : ops : add Dockerfile and Kustomization * multi layer Docker build with distroless last layer * Deployment + Service (no Ingress)

--- k8s_job_assistant/goapp/internal/kube/helpers.go
+++ k8s_job_assistant/goapp/internal/kube/helpers.go
@@ -2,7 +2,9 @@ package kube
 
 import (
 	"flag"
+	"fmt"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 	"path/filepath"
@@ -18,23 +20,45 @@ func newFalse() *bool {
 	return &b
 }
 
-// InitClient instantiate a Kubernetes client based on local kubeconfig.
+// InitClient instantiate a Kubernetes client based on local kubeconfig if exists
+// or default to in-cluster config
 func InitKubeClient() *kubernetes.Clientset {
-	var kubeconfig *string
+	var kubeconfigPath string
+
+	// Optional flag for kubeconfig
 	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		flag.StringVar(&kubeconfigPath, "kubeconfig", filepath.Join(home, ".kube", "config"),
+			"(optional) absolute path to the kubeconfig file")
 	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+		flag.StringVar(&kubeconfigPath, "kubeconfig", "",
+			"(optional) absolute path to the kubeconfig file")
 	}
 	flag.Parse()
 
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err.Error())
+	var config *rest.Config
+	var err error
+
+	// Use kubeconfig if explicitly provided or exists
+	if kubeconfigPath != "" {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+		if err != nil {
+			fmt.Printf("Warning: Failed to load kubeconfig: %v\nFalling back to in-cluster config.\n", err)
+		}
 	}
-	kubeClient, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
+
+	// If config is still nil, fallback to in-cluster config
+	if config == nil {
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			panic(fmt.Errorf("failed to load both kubeconfig and in-cluster config: %w", err))
+		}
 	}
-	return kubeClient
+
+	// Create clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(fmt.Errorf("failed to create Kubernetes client: %w", err))
+	}
+
+	return clientset
 }
--- k8s_job_assistant/goapp/main.go
+++ k8s_job_assistant/goapp/main.go
@@ -6,6 +6,8 @@ import (
 	"goapp/internal/kube"
 	"goapp/internal/service"
 	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
@@ -17,11 +19,16 @@ func main() {
 	jobService := service.NewJobService(jobManager)
 	handler.DecorateRouterWithJobHandlers(router, jobService)
 
-	// Serve Static React app
-	//r.LoadHTMLFiles("ui/index.html")
-	//r.GET("/", func(c *gin.Context) {
-	//	c.HTML(http.StatusOK, "index.html", gin.H{})
-	//})
+	//Serve Static React app
+	if _, err := os.Stat("ui/index.html"); err == nil {
+		router.LoadHTMLFiles("ui/index.html")
+		router.Static("/assets", "./ui/assets")
+		router.GET("/", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "index.html", gin.H{})
+		})
+	} else {
+		log.Println("ui/index.html not found, not serving static file")
+	}
 
 	// Start server
 	err := router.Run(":8080")
